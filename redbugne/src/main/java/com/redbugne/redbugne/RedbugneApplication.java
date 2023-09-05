762fa66b642495047cce10559311f72813d46edd
Gabriel <gabrielrobertodasilva087@gmail.com>
2023-09-04 21:30:54 -0300

    primeiro commit

---
+++ redbugne/src/main/java/com/redbugne/redbugne/RedbugneApplication.java
@@ -0,0 +1,15 @@
+package com.redbugne.redbugne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedbugneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedbugneApplication.class, args);
+
+		
+	}
+
+}
